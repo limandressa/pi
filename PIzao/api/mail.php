d7db8756f4c79b3272578aa6c520d3d392de77ca
limandressa <43620906+limandressa@users.noreply.github.com>
2018-12-04 13:02:49 -0200

    Add files via upload

---
+++ PIzao/api/mail.php
@@ -0,0 +1,39 @@
+<?php
+
+require "vendor/autoload.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+try {
+    //Server settings
+    $mail->SMTPDebug = 1;                                 // Enable verbose debug output
+    $mail->isSMTP(true);                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'pi2andressa@gmail.com';                 // SMTP username
+    $mail->Password = 'projetinho';                           // SMTP password
+    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('pi2andressa@gmail.com', 'Mailer');
+    $mail->addAddress('andressasts@hotmail.com', 'Andressa Lima');               // Name is optional
+    //$mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+	$mail->Subject = 'Alerta de enchentes e alagamenos';
+    $mail->AltBody = 'Este é um email padrão do aplicativo.';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+}
